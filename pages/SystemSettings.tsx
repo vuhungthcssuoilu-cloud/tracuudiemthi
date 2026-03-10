41a8a7d87d9bd8771e2808532472a9892a0c9827
vuhungthcssuoilu-cloud <vuhungthcssuoilu@gmail.com>
2026-03-10 20:21:38 +0700

    Update SystemSettings.tsx

--- pages/SystemSettings.tsx
+++ pages/SystemSettings.tsx
@@ -171,8 +171,8 @@ export const SystemSettings: React.FC = () => {
                         <input type="text" value={config.exam.schoolYear} onChange={(e) => updateExam('schoolYear', e.target.value)} className="w-full border p-3 rounded text-gray-600 font-bold focus:ring-2 focus:ring-[#337ab7]/20 outline-none" />
                     </div>
                     <div>
-                        <label className="block text-xs font-bold text-gray-500 mb-1.5 uppercase tracking-wider">Ngày công bố điểm (Hiển thị ngoài trang chủ)</label>
-                        <input type="text" value={config.exam.releaseDate || ''} onChange={(e) => updateExam('releaseDate', e.target.value)} className="w-full border p-3 rounded text-gray-800 font-bold focus:ring-2 focus:ring-[#337ab7]/20 outline-none" placeholder="Ví dụ: 20/05/2026" />
+                        <label className="block text-xs font-bold text-gray-500 mb-1.5 uppercase tracking-wider">Ngày, tháng, năm công bố điểm (Hiển thị ngoài trang chủ)</label>
+                        <input type="text" value={config.exam.releaseDate || ''} onChange={(e) => updateExam('releaseDate', e.target.value)} className="w-full border p-3 rounded text-gray-800 font-bold focus:ring-2 focus:ring-[#337ab7]/20 outline-none" placeholder="Ví dụ: Ngày 20 tháng 05 năm 2026" />
                     </div>
                     
                     {/* Cấu hình màu sắc Header */}
