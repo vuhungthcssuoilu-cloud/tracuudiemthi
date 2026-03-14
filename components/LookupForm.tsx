30b0d6cfc77f0aaaf80cd3bb55931793bf1ee8be
vuhungthcssuoilu-cloud <vuhungthcssuoilu@gmail.com>
2026-03-14 20:05:06 +0700

    Update LookupForm.tsx

--- components/LookupForm.tsx
+++ components/LookupForm.tsx
@@ -88,7 +88,7 @@ export const LookupForm: React.FC<LookupFormProps> = ({ onSearch, isLoading, err
               className="text-[16px] font-bold uppercase"
               style={{ color: config.exam.headerBackgroundColor }}
             >
-              Ngày công bố kết quả: {config.exam.releaseDate}
+              Ngày công bố kết quả thi: {config.exam.releaseDate}
             </p>
           )}
         </div>
@@ -153,7 +153,7 @@ export const LookupForm: React.FC<LookupFormProps> = ({ onSearch, isLoading, err
                   color: config.exam.headerTextColor 
                 }}
               >
-                {isLoading ? "..." : "Tra cứu"}
+                {isLoading ? "..." : "Tra cứu kết quả thi"}
               </button>
             </div>
 
