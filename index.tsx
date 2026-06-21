880eb01c3cf24517bc8c79a44e8f089a622a110d
vuhungthcssuoilu-cloud <vuhungthcssuoilu@gmail.com>
2026-06-21 21:06:30 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Xử lý chuyển hướng cho URL/admin khi dùng HashRouter
+if (window.location.pathname.startsWith('/admin')) {
+  const hashPath = window.location.pathname; // Ví dụ: /admin/login
+  window.location.replace('/#' + hashPath);
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
